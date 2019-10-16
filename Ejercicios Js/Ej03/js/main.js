b3f624c7d79102886a545b6ddbf058464320ca3b
CarlosPaz96 <38087007+CarlosPaz96@users.noreply.github.com>
2019-10-15 21:40:23 -0600

    Add files via upload

---
+++ Ejercicios Js/Ej03/js/main.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    let btn_dia1 = document.querySelector("#d1");
+    let btn_dia2 = document.querySelector("#d2");
+    let btn_dia3 = document.querySelector("#d3");
+    let btn_dia4 = document.querySelector("#d4");
+    let btn_dia5 = document.querySelector("#d5");
+
+    
+
+}
